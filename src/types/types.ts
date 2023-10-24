5db876b45f2520e87b9a54c01035adfa286ffde2
vicepalma <madahra@gmail.com>
2023-10-24 20:09:02 -0300

    react components with css

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,16 @@
-export interface MenuItem{
+export interface MenuItem {
     title: string;
     url: string;
-}
+    icon: string;
+  }
+  
+  export interface CardItem {
+    title: string;
+    image: string;
+    description: string;
+  }
+  
+  export interface faq {
+    question: string;
+    response: string;
+  }
